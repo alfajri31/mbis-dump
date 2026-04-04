17ff054e59a853213113b7bc2ff48d0d6805a09e
fajri <raden.alfajri@metrocom.co.id>
2026-04-04 15:08:22 +0700

    update

---
+++ m_divisions_202604041506-2-baris.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.m_divisions (id,code,"name",description,is_active,created_at,updated_at) VALUES
+	 ('M001',NULL,'Kantor Wilayah',NULL,true,'2026-04-03 17:43:47.964','2026-04-03 17:43:47.964'),
+	 ('M002',NULL,'Kantor Cabang',NULL,true,'2026-04-03 17:43:47.971','2026-04-03 17:43:47.971');
