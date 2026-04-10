58a6e29780da7fd5d25b94718cf7440ab0bbab13
fajri <raden.alfajri@metrocom.co.id>
2026-04-10 13:57:08 +0700

    update

---
+++ update-mbis-asset-bumn.sql
@@ -0,0 +1,67 @@
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-karya-logistik-nusantara.png' WHERE LOWER(company_name) LIKE 'pt%karya%logistik%nusantara%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-kawasan-industri-kujang-cikampek.png' WHERE LOWER(company_name) LIKE 'pt%kawasan%industri%kujang%cikampek%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-kawasan-industri-nusantara.png' WHERE LOWER(company_name) LIKE 'pt%kawasan%industri%nusantara%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-kawasan-industri-terpadu-batang.png' WHERE LOWER(company_name) LIKE 'pt%kawasan%industri%terpadu%batang%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-kimia-farma-trading--distribution.png' WHERE LOWER(company_name) LIKE 'pt%kimia%farma%trading%%distribution%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-komipo-pembangkit-jawa-bali.png' WHERE LOWER(company_name) LIKE 'pt%komipo%pembangkit%jawa%bali%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-lembaga-pendidikan-perkebunan-agro.png' WHERE LOWER(company_name) LIKE 'pt%lembaga%pendidikan%perkebunan%agro%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-len-railways-system.png' WHERE LOWER(company_name) LIKE 'pt%len%railways%system%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-lucas-djaja.png' WHERE LOWER(company_name) LIKE 'pt%lucas%djaja%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-mahakarya-abadi-prima.png' WHERE LOWER(company_name) LIKE 'pt%mahakarya%abadi%prima%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-manajemen-cbt-nusantara.png' WHERE LOWER(company_name) LIKE 'pt%manajemen%cbt%nusantara%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-marin-liza-farmasi.png' WHERE LOWER(company_name) LIKE 'pt%marin%liza%farmasi%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-medan-binjai-toll.png' WHERE LOWER(company_name) LIKE 'pt%medan%binjai%toll%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-merese-mandalika-nusantara.png' WHERE LOWER(company_name) LIKE 'pt%merese%mandalika%nusantara%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-mgpa-nusantara-jaya.png' WHERE LOWER(company_name) LIKE 'pt%mgpa%nusantara%jaya%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-musi-mitra-jaya.png' WHERE LOWER(company_name) LIKE 'pt%musi%mitra%jaya%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-napsindo-primatel-internasional.png' WHERE LOWER(company_name) LIKE 'pt%napsindo%primatel%internasional%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-nindya-tirta-unggul.png' WHERE LOWER(company_name) LIKE 'pt%nindya%tirta%unggul%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-nusa-dua-bekala.png' WHERE LOWER(company_name) LIKE 'pt%nusa%dua%bekala%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-nusantara-batulicin.png' WHERE LOWER(company_name) LIKE 'pt%nusantara%batulicin%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-nusantara-turbin--propulsi.png' WHERE LOWER(company_name) LIKE 'pt%nusantara%turbin%%propulsi%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-omni-inovasi-indonesia.png' WHERE LOWER(company_name) LIKE 'pt%omni%inovasi%indonesia%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-optima-nusa-tujuh.png' WHERE LOWER(company_name) LIKE 'pt%optima%nusa%tujuh%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-paguntaka-cahaya-nusantara.png' WHERE LOWER(company_name) LIKE 'pt%paguntaka%cahaya%nusantara%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-pbm-adhiguna-putera.png' WHERE LOWER(company_name) LIKE 'pt%pbm%adhiguna%putera%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-pejagan-pemalang-tol-road.png' WHERE LOWER(company_name) LIKE 'pt%pejagan%pemalang%tol%road%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-pelabuhan-bukit-prima.png' WHERE LOWER(company_name) LIKE 'pt%pelabuhan%bukit%prima%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-pemalang-batang-tol-road.png' WHERE LOWER(company_name) LIKE 'pt%pemalang%batang%tol%road%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-penajam-internasional-terminal.png' WHERE LOWER(company_name) LIKE 'pt%penajam%internasional%terminal%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-perkebunan-nusantara-ix.png' WHERE LOWER(company_name) LIKE 'pt%perkebunan%nusantara%ix%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-perkebunan-nusantara-viii.png' WHERE LOWER(company_name) LIKE 'pt%perkebunan%nusantara%viii%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-perkebunan-nusantara-vii.png' WHERE LOWER(company_name) LIKE 'pt%perkebunan%nusantara%vii%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-perkebunan-nusantara-vi.png' WHERE LOWER(company_name) LIKE 'pt%perkebunan%nusantara%vi%';
+
+UPDATE m_ecosystem_companies SET image_url = 'https://mbis.askrindo.co.id/api/v1/master/file/pt-perkebunan-nusantara-xiii.png' WHERE LOWER(company_name) LIKE 'pt%perkebunan%nusantara%xiii%';
