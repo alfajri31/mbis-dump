2ac9594e80275088e42c00d0f83b1c93576017af
fajri <raden.alfajri@metrocom.co.id>
2026-04-14 16:09:17 +0700

    update

---
+++ insert_m_cob.sql
@@ -0,0 +1,124 @@
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('ARK', 'Asuransi Rangka Kapal', 'AU', 'Asuransi Rangka Kapal', 1, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AR', 'Asuransi Rekayasa', 'AU', 'Asuransi Rekayasa', 2, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AS', 'Asuransi Satelit', 'AU', 'Asuransi Satelit', 3, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('ATG', 'Asuransi Tanggung Gugat', 'AU', 'Asuransi Tanggung Gugat', 4, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AA', 'Asuransi Aneka', 'AU', 'Asuransi Aneka', 5, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AHB', 'Asuransi Harta Benda', 'AU', 'Asuransi Harta Benda', 6, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AHE', 'Asuransi Heavy Equipment', 'AU', 'Asuransi Heavy Equipment', 7, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AK', 'Asuransi Kredit', 'AU', 'Asuransi Kredit', 8, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AKB', 'Asuransi Kendaraan Bermotor', 'AU', 'Asuransi Kendaraan Bermotor', 9, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AOG', 'Asuransi Oil & Gas', 'AU', 'Asuransi Oil & Gas', 10, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AP', 'Asuransi Pertanian', 'AU', 'Asuransi Pertanian', 11, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('APU', 'Asuransi Pesawat Udara', 'AU', 'Asuransi Pesawat Udara', 12, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('S', 'Suretyship', 'AU', 'Suretyship', 13, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('PK', 'PENJAMINAN KUR', 'AU', 'PENJAMINAN KUR', 14, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AU', 'Asuransi Umum', 'AU', 'Asuransi Umum', 15, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('P', 'Pengiriman', 'AU', 'Pengiriman', 16, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('A', 'AKM', 'AU', 'AKM', 17, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('K', 'Kontraktor', 'AU', 'Kontraktor', 18, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('SS', 'Surety ship', 'AU', 'Surety ship', 19, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('AKM', 'Asuransi Kredit Menengah', 'AU', 'Asuransi Kredit Menengah', 20, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('F', 'FLEXAS', 'AU', 'FLEXAS', 21, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('SB', 'Surety bond', 'AU', 'Surety bond', 22, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('WAEL', 'WORKMENS AND EMPLOYERS LIABILITY', 'AU', 'WORKMENS AND EMPLOYERS LIABILITY', 23, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('PS', 'Pa siswa', 'AU', 'Pa siswa', 24, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_cob
+(id, cob_name, lob_code, description, sort_order, is_active, created_at, updated_at)
+VALUES('M', 'Moc', 'AU', 'Moc', 25, true, now(), now())
+ON CONFLICT (id) DO NOTHING;
---
+++ insert_m_lob.sql
@@ -0,0 +1,124 @@
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('ARK', 'Asuransi Rangka Kapal', 'Asuransi Rangka Kapal', 1, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AR', 'Asuransi Rekayasa', 'Asuransi Rekayasa', 2, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AS', 'Asuransi Satelit', 'Asuransi Satelit', 3, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('ATG', 'Asuransi Tanggung Gugat', 'Asuransi Tanggung Gugat', 4, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AA', 'Asuransi Aneka', 'Asuransi Aneka', 5, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AHB', 'Asuransi Harta Benda', 'Asuransi Harta Benda', 6, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AHE', 'Asuransi Heavy Equipment', 'Asuransi Heavy Equipment', 7, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AK', 'Asuransi Kredit', 'Asuransi Kredit', 8, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AKB', 'Asuransi Kendaraan Bermotor', 'Asuransi Kendaraan Bermotor', 9, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AOG', 'Asuransi Oil & Gas', 'Asuransi Oil & Gas', 10, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AP', 'Asuransi Pertanian', 'Asuransi Pertanian', 11, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('APU', 'Asuransi Pesawat Udara', 'Asuransi Pesawat Udara', 12, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('S', 'Suretyship', 'Suretyship', 13, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('PK', 'PENJAMINAN KUR', 'PENJAMINAN KUR', 14, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AU', 'Asuransi Umum', 'Asuransi Umum', 15, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('P', 'Pengiriman', 'Pengiriman', 16, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('A', 'AKM', 'AKM', 17, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('K', 'Kontraktor', 'Kontraktor', 18, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('SS', 'Surety ship', 'Surety ship', 19, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('AKM', 'Asuransi Kredit Menengah', 'Asuransi Kredit Menengah', 20, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('F', 'FLEXAS', 'FLEXAS', 21, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('SB', 'Surety bond', 'Surety bond', 22, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('WAEL', 'WORKMENS AND EMPLOYERS LIABILITY', 'WORKMENS AND EMPLOYERS LIABILITY', 23, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('PS', 'Pa siswa', 'Pa siswa', 24, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.m_lob
+(id, lob_name, description, sort_order, is_active, created_at, updated_at)
+VALUES('M', 'Moc', 'Moc', 25, true, '2026-04-14 08:41:07', '2026-04-14 08:41:07')
+ON CONFLICT (id) DO NOTHING;
